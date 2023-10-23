d6bb2a317fc299ec978d7bc70c0e562c229d13c2
Susana Delgado <susanadp@Susanas-MacBook-Pro.local>
2023-10-23 16:00:54 -0400

    subiendo archivos

---
+++ pass.js
@@ -0,0 +1,14 @@
+function pass() {
+    
+    let c="losGatosSonLoMejor5689";
+    
+    
+
+    if(document.form.password.value==c){
+
+    window.location.href = "codoacodo.html";
+}
+else{
+alert("Error: esa no es la contraseña. Por favor ingrese la contraseña correcta");
+}
+}
